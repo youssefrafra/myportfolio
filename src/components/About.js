1d15618a30e98b9a29c5621af03f1a4275bd4b5b
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-08-06 18:05:30 +0200

    70% of portfolio

---
+++ src/components/About.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import nurali from "../nurali-hills.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+export default function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>
+
+  console.log(author)
+  return (
+    <main className="relative">
+      <img src={nurali} alt="Nurali Hills" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-600 bg-opacity-50 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            alt={author.name}
+            className="rounded-full w-32 h-32 lg:w-64 lg:h-64 mr-8"
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hi there, I'm{" "}
+              <span className="text-white">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="w9mqc294"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from '../smoky-morning.jpg'
+
+export default function Home(){
+    return (
+        <main>
+            <img src={image} alt="Smoky Morning" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi, I'm Youssef.</h1>
+            </section>
+        </main>
+    )
+}
